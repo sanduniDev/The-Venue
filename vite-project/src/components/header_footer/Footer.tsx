8dfb302dc54a76fd7410c0eebd17f7133094c644
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-09-20 15:26:35 +0530

    feat: footer

--- vite-project/src/components/header_footer/Footer.tsx
+++ vite-project/src/components/header_footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="bck_red">
             <Fade triggerOnce delay={500}>
                 <div className="font_righteous footer_logo_venue">The Venue</div>
-                <div className="footer_copyright">The venue 2023 all rights reserved</div>
+                <div className="footer_copyright">The venue @2023 all rights reserved</div>
             </Fade>
         </footer>
     )
